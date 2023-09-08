fad3a324f07e5fe5b6c3f11121127784d8585f9c
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-09-08 15:51:08 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
 
-// QUESTA IMPORTAZIONE NON FUNZIONA:
-// const path = require('path');
-
 export default defineConfig({
     plugins: [
         laravel({
@@ -17,9 +14,9 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            //Aggiungiamo un alias per la cartella /resources/
+            //Alias per la cartella /resources/
             '~resources': '/resources/',
-            //Aggiungiamo un alias per la cartella /node_modules/bootstrap/
+            //Alias per la cartella /node_modules/bootstrap/
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
         }
     },
